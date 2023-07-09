777f40f543e80f113acdef452a3b59f189c65e7b
Isaac Ramirez Herrera <joseisaacr@microsoft.com>
2023-07-08 21:25:53 -0600

    First working version

---
+++ src/main.js
@@ -0,0 +1,89 @@
+const {
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
+} = require("@aws-sdk/client-s3");
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
+const { transcodeFile } = require("./transcoder");
+
+const client = new S3Client({ region: "us-east-1" });
+
+const temp_folder = "video-transcode";
+
+async function main(srcBucket, file, destBucket, destKey) {
+  try {
+    const outputFolder =
+      "/var/folders/7_/tjg1zhvj4d5b_y71wd06x5wc0000gn/T/video-transcode6wBQKD/output/";
+    // const outputFolder = await createTempFolder();
+    // const downloadedFilePath = await downloadFile(
+    //   srcBucket,
+    //   file,
+    //   outputFolder
+    // );
+    // await transcodeFile(downloadedFilePath, outputFolder);
+    await uploadTranscodedVideo(outputFolder, destBucket, destKey);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function downloadFile(bucket, key, outputFolder) {
+  console.log(`Downloading file ${bucket}/${key}...`);
+  const command = new GetObjectCommand({
+    Bucket: bucket,
+    Key: key,
+  });
+  const absoluteFilePath = path.join(outputFolder, key);
+  const writeStream = fs.createWriteStream(absoluteFilePath, {
+    encoding: "utf8",
+  });
+  const response = await client.send(command);
+  return new Promise((resolve, reject) => {
+    response.Body.pipe(writeStream)
+      .on("error", (err) => reject(err))
+      .on("close", () => resolve(absoluteFilePath));
+  });
+}
+
+async function uploadTranscodedVideo(outputFolder, bucket, destKey) {
+  console.log(`Uploading folder ${outputFolder}...`);
+  const filesToUpload = fs.readdirSync(outputFolder);
+  if (filesToUpload.length === 0) {
+    console.error("No files to upload");
+    process.exit(1);
+  }
+  const promises = [];
+  filesToUpload.forEach((file) => {
+    promises.push(
+      uploadSingleFile(outputFolder, file, bucket, `${destKey}/${file}`)
+    );
+  });
+  return Promise.all(promises);
+}
+
+async function createTempFolder() {
+  const tempFolder = await fs.mkdtempSync(path.join(os.tmpdir(), temp_folder));
+  console.log(`Output folder path: ${tempFolder}`);
+  return tempFolder;
+}
+
+async function uploadSingleFile(srcFolder, fileName, bucket, key) {
+  console.log(`Uploading file ${fileName} to bucket ${bucket}...`);
+  const fileStream = fs.createReadStream(path.join(srcFolder, fileName));
+  const command = new PutObjectCommand({
+    Bucket: bucket,
+    Key: key,
+    Body: fileStream,
+  });
+  await client.send(command);
+  fileStream.close();
+}
+
+main(
+  "upc-video-transcode-input",
+  "world.mp4",
+  "upc-video-transcode-output",
+  "world"
+);
---
+++ src/transcoder.js
@@ -0,0 +1,108 @@
+/**
+ * Based on https://blog.infireal.com/2018/04/mpeg-dash-with-only-ffmpeg/
+ */
+const path = require("path");
+const ffmpeg = require("fluent-ffmpeg");
+const os = require("os");
+var fs = require("fs");
+
+if (os.platform() == "win32") {
+  const binarypath = path.resolve("./ffmpeg/bin/");
+  const FfmpegPath = path.join(binarypath, "ffmpeg.exe");
+  try {
+    const FfmpegPathInfo = fs.statSync(FfmpegPath);
+  } catch (err) {
+    throw err;
+  }
+  ffmpeg.setFfmpegPath(FfmpegPath);
+  ffmpeg.setFfprobePath(path.join(binarypath, "ffprobe.exe"));
+}
+
+async function transcodeFile(inputVideoPath, outputFolder) {
+  const targetdir = path.join(outputFolder, "output");
+  createTargetDir(targetdir);
+  runFFMpeg(targetdir, inputVideoPath);
+}
+
+function createTargetDir(targetdir) {
+  try {
+    fs.statSync(targetdir);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      fs.mkdirSync(targetdir);
+    } else {
+      throw err;
+    }
+  }
+}
+
+function runFFMpeg(targetdir, inputVideoPath) {
+  const name = path.basename(inputVideoPath, path.extname(inputVideoPath));
+  const sourcefn = path.resolve(inputVideoPath);
+  const targetfn = path.join(targetdir, `${name}.mpd`);
+  const proc = ffmpeg({
+    source: sourcefn,
+    cwd: targetdir,
+  });
+  setArguments(proc, targetfn);
+  proc.on("start", function (commandLine) {
+    console.log("progress", "Running FFmpeg with command: " + commandLine);
+  });
+  return new Promise((resolve, reject) => {
+    proc
+      .on("progress", function (info) {
+        console.log("progress", info);
+      })
+      .on("end", function () {
+        console.log("complete");
+        resolve();
+      })
+      .on("error", function (err) {
+        console.log("error", err);
+        reject();
+      });
+    proc.run();
+  });
+}
+
+function setArguments(process, targetfn) {
+  const sizes = [
+    [240, 350],
+    [480, 700],
+    [720, 2500],
+    [1080, 3500],
+  ];
+  proc
+    .output(targetfn)
+    .format("dash")
+    .videoCodec("libx264")
+    .audioCodec("aac")
+    .audioChannels(2)
+    .audioFrequency(44100)
+    .outputOptions([
+      "-preset veryfast",
+      "-keyint_min 60",
+      "-g 60",
+      "-sc_threshold 0",
+      "-profile:v main",
+      "-use_template 1",
+      "-use_timeline 1",
+      "-b_strategy 0",
+      "-bf 1",
+      "-map 0:a",
+      "-b:a 96k",
+    ]);
+
+  for (const size of sizes) {
+    let index = sizes.indexOf(size);
+    proc.outputOptions([
+      `-filter_complex [0]format=pix_fmts=yuv420p[temp${index}];[temp${index}]scale=-2:${size[0]}[A${index}]`,
+      `-map [A${index}]:v`,
+      `-b:v:${index} ${size[1]}k`,
+    ]);
+  }
+}
+
+module.exports = {
+  transcodeFile,
+};
